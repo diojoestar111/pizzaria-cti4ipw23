100e59a34f6a7b6a3dcbfb8cae3630d2edffb436
diojoestar111 <108557111+diojoestar111@users.noreply.github.com>
2023-03-31 20:13:39 -0300

    Add files via upload

---
+++ pizzaria-cti4ipw23-main/pizzaria-cti4ipw23-main/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+    <title>Pizzaria Sua Fome Minha Alegria</title>
+  </head>
+  <body>
+    <header class="text-white d-flex">
+      <div class="container d-flex justify-content-between align-items-center">
+		<img src="img/logo.png" alt="Logo Pizzaria" />
+
+    </div>
+    <nav>
+      <ul class="menu list-inline">
+        <li class="menu list-inline-item"><a haref="#">Home</a></li>
+        <li class="menu list-inline-item"><a haref="#">Sobre</a></li>
+        <li class="menu list-inline-item"><a haref="#">Cardápio</a></li>
+        <li class="menu list-inline-item"><a haref="#">Contato</a></li>
+      </ul>
+    </nav>
+	</header>
+  <section class="container">
+      <div class="row">
+        <nav class="col-md-2">
+          <ul class="list-unstyled">
+          <li><a haref="#">Home</a></li>
+          <li><a haref="q1.html">Questão 1</a></li>
+          <li><a haref="q2.html">Questão 2</a></li>
+          <li><a haref="q3.html">Questão 3</a></li> 
+          <li><a haref="q4.html">Questão 4</a></li>   
+          </ul>
+        </nav>
+        <main class="col-md-9">
+          <h1>Home</h1>
+          <p>
+            ipsum
+          </p>
+        </main>
+      </div>
+  </section>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
